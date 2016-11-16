186f38b7544c173f5ba74a3396e9472f167a7831
Keith Brisson <kbrisson@gmail.com>
2016-11-16 13:19:23 -0500

    Fix syntax error

--- behavior/scripts/lib/company/provideCompanySummary.js
+++ behavior/scripts/lib/company/provideCompanySummary.js
@@ -69,7 +69,7 @@ module.exports = (client, state) => {
               ticker: result.ticker,
             },
           },
-        },)
+        })
 
         actions.push({
           type: 'postback',
